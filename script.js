472459b743055fbd661a0b258bf4ab1b368a11e1
Van <152158486+ivanajeo@users.noreply.github.com>
2024-07-27 17:02:50 +0700

    Refactor squareBtn event listener, add createSquares function

--- script.js
+++ script.js
@@ -9,32 +9,38 @@ function getRandomBgColor() {
     return `rgba(${r}, ${g}, ${b}, `;
 }
 
-squareBtn.addEventListener("click", function() {
-    drawingPad.textContent = "";
+function createSquares(squaresPerRow) {
+    drawingPad.innerHTML = "";
 
-    let squaresPerRow;
-    do {
-        squaresPerRow = parseInt(prompt("number of squares? (max 100"), 10); 
-    } while (squaresPerRow > 100);
-    
-    let totalSquares = squaresPerRow**2;
-    let squareWidth = `calc(100% / ${squaresPerRow})`;
-    for (let i = 0; i < totalSquares; i++) {
-        const square = document.createElement("div");
+    const totalSquares = squaresPerRow ** 2;
+    const squareWidth = `calc(100% / ${squaresPerRow})`;
+
+    for(let i = 0; i < totalSquares; i++) {
+        let square = document.createElement("div");
         square.classList.add("square");
         square.style.flexBasis = squareWidth;
 
-        let randomBgColor = getRandomBgColor();
-        
+        const randomBgColor = getRandomBgColor();
         let hoverCount = 0;
-        square.addEventListener("mouseenter", function() {
+        square.addEventListener("mouseenter", () => {
             hoverCount += 1;
             let newOpacity = Math.min(hoverCount * 0.25, 1);
-            let bgColor = `${randomBgColor}${newOpacity})`
-            square.style.backgroundColor = bgColor;
-        })
-    drawingPad.appendChild(square);
-};
-})
 
-    
+            square.style.backgroundColor = `${randomBgColor}${newOpacity}`;
+        })
+        drawingPad.appendChild(square);
+    }
+}
+   
+
+squareBtn.addEventListener("click", () => {
+    let squaresPerRow;
+    do {
+        squaresPerRow = parseInt(prompt("Number of squares? (max 100), 10"));
+    } while (isNaN(squaresPerRow) || squaresPerRow > 100);
+
+    if (squaresPerRow) {
+        createSquares(squaresPerRow);
+        console.log("create");
+    }
+})
