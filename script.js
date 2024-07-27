147eb704c6a7913cac552c475998438328afa1f9
Van <152158486+ivanajeo@users.noreply.github.com>
2024-07-27 15:39:19 +0700

    add event listener to squareBtn

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 const drawingPad = document.querySelector(".drawingPad");
+const squareBtn = document.querySelector(".squareBtn");
 
 
-let squaresPerRow = parseInt(prompt("number of squares?"));
-let totalSquares = squaresPerRow**2;
-let squareWidth = `calc(100% / ${squaresPerRow})`;
-for (let i = 0; i < totalSquares; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    square.style.flexBasis = squareWidth;
+squareBtn.addEventListener("click", function() {
+    drawingPad.textContent = "";
+    let squaresPerRow = parseInt(prompt("number of squares?"));
+    let totalSquares = squaresPerRow**2;
+    let squareWidth = `calc(100% / ${squaresPerRow})`;
+    for (let i = 0; i < totalSquares; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.flexBasis = squareWidth;
 
-    let hoverCount = 0;
-    square.addEventListener("mouseenter", function() {
-        hoverCount += 1;
-        let newOpacity = Math.min(hoverCount * 0.1, 1);
-        square.style.backgroundColor = `rgba(0, 255, 0, ${newOpacity})`;
-    })
+        let hoverCount = 0;
+        square.addEventListener("mouseenter", function() {
+            hoverCount += 1;
+            let newOpacity = Math.min(hoverCount * 0.1, 1);
+            square.style.backgroundColor = `rgba(0, 255, 0, ${newOpacity})`;
+        })
     drawingPad.appendChild(square);
-
-
 };
+})
 
+    
