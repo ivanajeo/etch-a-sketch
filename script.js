58a843ca9657e735c05a4114d11faf270c45c03f
Van <152158486+ivanajeo@users.noreply.github.com>
2024-07-29 15:11:55 +0700

    Add comments

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const clearBtn = document.querySelector(".clearBtn");
 const colorBlackBtn = document.querySelector(".colorBlackBtn");
 const colorRandomBtn = document.querySelector(".colorRandomBtn");
 
+// variables to track the current mode of the drawing pad
 let eraserMode = false;
 let colorBlackMode = true;
 let colorRandomMode = false;
@@ -16,22 +17,26 @@ function getRandomBgColor() {
     return `rgba(${r}, ${g}, ${b})`;
 }
 
+// update the button states to indicate active/inactive status
 function updateButtonState(activeBtn, inactiveBtn) {
     activeBtn.classList.add("colorBtn-active");
     inactiveBtn.classList.remove("colorBtn-active");
 }
 
+// create a grid of squares in the drawing pad
 function createSquares(squaresPerRow) {
-    drawingPad.innerHTML = "";
+    drawingPad.innerHTML = ""; // clear existing squares
 
     const totalSquares = squaresPerRow ** 2;
     const squareWidth = `calc(100% / ${squaresPerRow})`;
     
+    // loop to create each square and add event listener
     for(let i = 0; i < totalSquares; i++) {
         let square = document.createElement("div");
         square.classList.add("square");
         square.style.flexBasis = squareWidth;
        
+        // event listener to change bgColor
         square.addEventListener("mouseenter", () => {
             if (eraserMode) {
                 square.style.backgroundColor = "";
@@ -45,7 +50,8 @@ function createSquares(squaresPerRow) {
     }
 }
    
-
+// event listener for the squareBtn to prompt user for number of squares
+// and create the squares
 squareBtn.addEventListener("click", () => {
     let squaresPerRow;
     do {
@@ -57,23 +63,27 @@ squareBtn.addEventListener("click", () => {
     }
 });
 
+// event listener to switch to colorBlackMode
 colorBlackBtn.addEventListener("click", () => {
     colorBlackMode = true;
     colorRandomMode = false;
     updateButtonState(colorBlackBtn, colorRandomBtn);
 });
 
+// event listener to switch to colorRandomMode
 colorRandomBtn.addEventListener("click", () => {
     colorRandomMode = true;
     colorBlackMode = false;
     updateButtonState(colorRandomBtn, colorBlackBtn);
 });
 
+// event listener to toggle eraserMode and its class
 eraserBtn.addEventListener("click", () => {
     eraserMode = !eraserMode;
     eraserBtn.classList.toggle("eraserBtn-active", eraserMode);
 });
 
+// event listener to reset drawing pad
 clearBtn.addEventListener("click", () => {
     const squares = document.querySelectorAll(".square");
     squares.forEach((square) => {
@@ -82,4 +92,5 @@ clearBtn.addEventListener("click", () => {
 });
 
 
-createSquares(10); // Initialize with 10 squares per row
+// Initialize with 10 squares per row
+createSquares(10); 
